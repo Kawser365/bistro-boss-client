f18b4969a59f861d5bc575a0b24bfde06963ea83
Kawser365 <mdkamrulhasan.khk@gmail.com>
2023-10-26 00:29:19 +0600

    add cheffRecommend component

---
+++ src/components/shared/RecommendMenu/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { Button, Card } from "antd";
+import Image from "next/image";
+const { Meta } = Card;
+const RecommendMenu = ({ item }: any) => {
+  const { name, image, recipe } = item;
+  return (
+    <div>
+      <Card
+        style={{ width: "100%" }}
+        cover={
+          <Image
+            alt="example"
+            height={280}
+            width={340}
+            src={image}
+            style={{ objectFit: "cover" }}
+          />
+        }
+      >
+        <Meta
+          style={{ textAlign: "center" }}
+          title={name}
+          description={recipe}
+        />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            textTransform: "uppercase",
+            marginTop: "20px",
+          }}
+        >
+          <Button
+            type="primary"
+            style={{
+              backgroundColor: "#fbb034",
+              color: "#fff",
+              height: "35px",
+            }}
+          >
+            Add to cart
+          </Button>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default RecommendMenu;
--- src/pages/Home/page.tsx
+++ src/pages/Home/page.tsx
@@ -2,6 +2,7 @@ import Banner from "./Banner";
 import Bistro from "./Bistro";
 import CallUs from "./CallUs";
 import Category from "./Category";
+import CheffRecommend from "./CheffRecommend";
 import CustomerReview from "./CustomerReview";
 import Featured from "./Featured";
 import PopularMenu from "./PopularMenu";
@@ -15,6 +16,7 @@ const Home = () => {
         <Bistro></Bistro>
         <PopularMenu></PopularMenu>
         <CallUs></CallUs>
+        <CheffRecommend></CheffRecommend>
         <Featured></Featured>
         <CustomerReview></CustomerReview>
       </div>
